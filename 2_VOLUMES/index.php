74bc866596bf1e6c9307af861cd59e5912f1e9ea
tadeubdossantos <tadeub.dossantos@gmail.com>
2023-08-31 20:03:18 -0300

    up do conteudo estudado ate o compose

---
+++ 2_VOLUMES/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mensagens</title>
+</head>
+<body>
+    <h1>Escreva sua mensagem! BIND MOUNT</h1>
+    <form action="process.php" method="post">
+        <input type="text" name="message" id="message">
+        <input type="submit" value="Enviar mensagem">
+    </form>
+</body>
+</html>
